e867c60c566e25c9c7fa3afa7e60487884445fba
Julia <bartleyjulia@gmail.com>
2018-02-07 03:57:24 +0000

    Added knex and migration file

---
+++ knexfile.js
@@ -0,0 +1,20 @@
+// Update with your config settings.
+
+const settings = require("./settings");
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      user     : settings.user,
+      password : settings.password,
+      database : settings.database,
+      host     : settings.hostname,
+      port     : settings.port,
+      ssl      : settings.ssl
+    }
+  }
+
+
+};
