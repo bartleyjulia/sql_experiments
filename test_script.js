27c5efc6cfae504d4c1e3e3c4904c90676771b7d
Julia <bartleyjulia@gmail.com>
2018-02-07 01:38:30 +0000

    Refactored test_js to use knex

--- test_script.js
+++ test_script.js
@@ -1,38 +1,74 @@
 const lookUp = process.argv[2];
 
+console.log("Searching....");
 
-const pg = require("pg");
 const settings = require("./settings"); // settings.json
 
-const client = new pg.Client({
+var knex = require('knex')({
+  client: 'pg',
+  connection: {
   user     : settings.user,
   password : settings.password,
   database : settings.database,
   host     : settings.hostname,
   port     : settings.port,
   ssl      : settings.ssl
+  }
 });
 
-console.log("Searching....");
-client.connect((err) => {
+knex.select('*').from('famous_people').where('last_name', lookUp).orWhere('first_name', lookUp).asCallback(function (err, result){
   if (err) {
-    return console.error("Connection Error", err);
-  }
-  client.query("SELECT * FROM famous_people WHERE last_name = $1::text OR first_name = $1::text ", [lookUp], (err, result) => {
-    if (err) {
       return console.error("error running query", err);
     }
 
-    console.log(`Found ${result.rows.length} person(s) by the name ${lookUp}:`);
-    for (const row of result.rows) {
-      var d = new Date(row.birthdate);
+  console.log(`Found ${result.length} person(s) by the name ${lookUp}:`);
+    for ( let i = 0; i < result.length; i++) {
+      var d = new Date(result[i].birthdate);
       var curr_day = d.getDate();
       var curr_month = ('0' + (d.getMonth() + 1)).slice(-2);
       var curr_year = d.getFullYear();
       var convertedBDay = curr_year + "-" + curr_month + "-" + curr_day;
-      console.log(`-${row.id}: ${row.first_name} ${row.last_name} born \'${convertedBDay}\'`);
+      console.log(`-${result[i].id}: ${result[i].first_name} ${result[i].last_name} born \'${convertedBDay}\'`);
     }
 
-    client.end();
-  });
 });
+
+
+// const lookUp = process.argv[2];
+
+
+// const pg = require("pg");
+// const settings = require("./settings"); // settings.json
+
+// const client = new pg.Client({
+//   user     : settings.user,
+//   password : settings.password,
+//   database : settings.database,
+//   host     : settings.hostname,
+//   port     : settings.port,
+//   ssl      : settings.ssl
+// });
+
+// console.log("Searching....");
+// client.connect((err) => {
+//   if (err) {
+//     return console.error("Connection Error", err);
+//   }
+//   client.query("SELECT * FROM famous_people WHERE last_name = $1::text OR first_name = $1::text ", [lookUp], (err, result) => {
+//     if (err) {
+//       return console.error("error running query", err);
+//     }
+
+//     console.log(`Found ${result.rows.length} person(s) by the name ${lookUp}:`);
+//     for (const row of result.rows) {
+//       var d = new Date(row.birthdate);
+//       var curr_day = d.getDate();
+//       var curr_month = ('0' + (d.getMonth() + 1)).slice(-2);
+//       var curr_year = d.getFullYear();
+//       var convertedBDay = curr_year + "-" + curr_month + "-" + curr_day;
+//       console.log(`-${row.id}: ${row.first_name} ${row.last_name} born \'${convertedBDay}\'`);
+//     }
+
+//     client.end();
+//   });
+// });
